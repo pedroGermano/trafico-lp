a60f49878903a177c6bdf64df7b7a7edf0b8fc9a
Pedro Germano <pedrogermano232@gmail.com>
2022-09-17 22:28:37 -0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Nav from '../components/Nav';
+import NavMobile from '../components/NavMobile';
+
+import { HiMenu } from 'react-icons/hi';
+
+const Header = ({ headerData, navData }) => {
+  const [header, setHeader] = useState(false);
+  const [navMobile, setNavMobile] = useState(false);
+  const { logoImgV1, logoImgV2, btnText } = headerData;
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 80 ? setHeader(true) : setHeader(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        header
+          ? 'bg-white px-3 py-3 rounded-md drop-shadow-primary'
+          : 'py-[40px]'
+      } fixed w-full left-0 right-0 mx-auto max-w-[90vw] lg:max-w-[1120px] z-20 flex justify-between items-center transition-all duration-500`}
+    >
+      {/* logo v1 */}
+      <Link href={'/'}>
+        <a>
+          <Image
+            src={`${header ? logoImgV2 : logoImgV1}`}
+            width={header ? 180 : 212}
+            height={50}
+            alt="logo"
+          />
+        </a>
+      </Link>
+      <div className='hidden lg:flex gap-x-[96px]'>
+        {/* nav */}
+        <Nav navData={navData} header={header} />
+        {/* btn */}
+        <button className='btn'>{btnText}</button>
+      </div>
+
+      <div
+        onClick={() => setNavMobile(!navMobile)}
+        className='lg:hidden cursor-pointer'
+      >
+        <HiMenu className='text-4xl text-accent-hover' />
+      </div>
+
+      <div
+        className={`${
+          navMobile ? 'max-h-[154px]' : 'max-h-0'
+        } lg:hidden absolute top-full mt-2 w-full left-0 rounded-md overflow-hidden shadow-2xl  transition-all`}
+      >
+        <NavMobile navData={navData} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/NavMobile.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const NavMobile = ({navData}) => {
+  return (
+    <nav className="bg-accent w-full p-4">
+      <ul className="flex flex-col gap-y-6">
+        {navData.map((item, idx) => {
+          const {name, href} = item;
+          return <li key={idx}>
+            <Link href={href}>
+              <a className="text-white hover:text-accent" href="#">{name}</a>
+            </Link>
+          </li>
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMobile;
